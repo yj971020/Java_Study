e4ec137192767e2f68bb0c93ac80e01e99e79c5d
김예진 <yj971020@naver.com>
2021-08-13 19:43:09 +0900

    8/13

--- Chapter11_API/src/object/StringcharAtExample.java
+++ Chapter11_API/src/object/StringcharAtExample.java
@@ -4,6 +4,17 @@ public class StringcharAtExample {
 
 	public static void main(String[] args) {
 
+		String ssn="010624-1230123";
+		char sex=ssn.charAt(7); // 7번 인덱스 *인덱스 : 0부터 시작
+		switch(sex) {
+		case '1':
+		case '3': System.out.println(" 남자입니다.");
+		break;
+		case '2':
+		case '4':
+			System.out.println("여자입니다.");
+			break;
+		}
 	}
 
 }
