ba1305659804dc874e154a484be390baab383375
김예진 <yj971020@naver.com>
2021-08-28 15:02:48 +0900

    8/28

--- Chapter10_Exception/src/ch10_2_exception_handling/CatchOrderExample.java
+++ Chapter10_Exception/src/ch10_2_exception_handling/CatchOrderExample.java
@@ -3,7 +3,7 @@ package ch10_2_exception_handling;
 public class CatchOrderExample {
 
 	public static void main(String[] args) {
-		try {
+		try{
 
 			String data1 = args[0];
 			String data2 = args[1];
