f94e0e3d38c4cd4ac7b04ba118d17fdf5da93162
김예진 <yj971020@naver.com>
2021-08-29 13:01:08 +0900

    8/29

--- Chapter08_Interface/src/ch08_1_interface/MyClassExample.java
+++ Chapter08_Interface/src/ch08_1_interface/MyClassExample.java
@@ -27,21 +27,21 @@ class Myclass{
 
 public class MyClassExample {
 	public static void main(String[] args) {
-		System.out.println("1)------------------------");
+		System.out.println("1) ------------------------");
 		
 		Myclass myclass1= new Myclass();
 		myclass1.rc.turnOn();
 		myclass1.rc.setVolume(5);
 		
-		System.out.println("2)------------------------");
+		System.out.println("2) ------------------------");
 		
 		Myclass myclass2=new Myclass(new Audio());
-		System.out.println("3)------------------------");
+		System.out.println("3) ------------------------");
 		
 		Myclass myclass3=new Myclass();
 		myclass3.methodA();
 		
-		System.out.println("4)------------------------");
+		System.out.println("4) ------------------------");
 	}
 
 }
